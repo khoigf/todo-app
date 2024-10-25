e84e584a71331339036435b110c1eda9c6fd3545
khoigf <khoihuy7a@gmail.com>
2024-10-25 09:17:24 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+let todos = [
+    {
+        "task": "Learn DevOps"
+    },
+    {
+        "task": "Code API"
+    }
+        
+];
+
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+app.post('/todos', (req, res) => {
+    const { task } = req.body;
+    todos.push({ id: todos.length + 1, task });
+    res.json({ message: 'Task added successfully' });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
