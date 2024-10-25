3b7882df1e8cf2be6875f21fbb301fb05785d78d
khoigf <khoihuy7a@gmail.com>
2024-10-25 17:23:30 +0700

    Add port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-
+const PORT = process.env.PORT || 3000;
 let todos = [
     {
         "task": "Learn DevOps"
@@ -24,6 +24,6 @@ app.post('/todos', (req, res) => {
     res.json({ message: 'Task added successfully' });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
